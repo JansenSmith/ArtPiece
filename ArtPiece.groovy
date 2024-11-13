0c023dfdc7a4af39e53c9b4e3e9ff681c538cb91
Jansen Smith <jansen@bstarengineering.com>
2024-11-12 19:27:13 -0500

    ready to run mechEng piece

--- ArtPiece.groovy
+++ ArtPiece.groovy
@@ -1,6 +1,13 @@
+import eu.mihosoft.vrl.v3d.*
 
 def name = "mechEng"
 
+//if (name.equals("mechEng")){
+//	println "YES"
+//} else{
+//	println "NO"
+//}
+//return name
 
 println "Loading piece STL from repo"
 // Load an STL file from a git repo
@@ -26,6 +33,52 @@ CSG sig =  (CSG)ScriptingEngine.gitScriptRun(
 	                              null// no parameters (see next tutorial)
                         )
 
-return [piece, desc, sig]
 
-//CSG pieceSTL = 
+println "Moving piece into position"
+piece = piece.toXMin().toYMin().toZMin()
+
+println "Moving description into position"
+desc = desc.mirrorx().movex(piece.totalX)
+
+println "Moving signature into position"
+sig = sig.mirrorx()
+
+println "Combine description and signature geometries"
+CSG combin = sig.union(desc)
+
+println "Removing description and signature geometries from the piece"
+piece = piece.difference(combin)
+//piece = piece.difference(sig)
+
+
+piece = piece.setColor(javafx.scene.paint.Color.DARKGRAY)
+			.setName(name+"_piece")
+			.addAssemblyStep(0, new Transform())
+			.setManufacturing({ toMfg ->
+				return toMfg
+						//.rotx(180)// fix the orientation
+						//.toZMin()//move it down to the flat surface
+			})
+
+desc = desc.setColor(javafx.scene.paint.Color.DARKRED)
+			.setName(name+"_desc")
+			.addAssemblyStep(0, new Transform())
+			.setManufacturing({ toMfg ->
+				return toMfg
+						//.rotx(180)// fix the orientation
+						//.toZMin()//move it down to the flat surface
+			})
+
+sig = sig.setColor(javafx.scene.paint.Color.DARKRED)
+			.setName(name+"_sig")
+			.addAssemblyStep(0, new Transform())
+			.setManufacturing({ toMfg ->
+				return toMfg
+						//.rotx(180)// fix the orientation
+						//.toZMin()//move it down to the flat surface
+			})
+
+def ret = [piece, desc, sig]
+
+return ret
+
